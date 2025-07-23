ea29d5f7e335d575ad8d392d845b0d15f0a3ed3e
Ganesan G <ganesang@sidbi.in>
2025-07-23 06:54:17 +0530

    INELIGIBLE user marking & relatd changes

--- src/features/signUp/SignUpComponent.tsx
+++ src/features/signUp/SignUpComponent.tsx
@@ -361,7 +361,7 @@ const SignUp = () => {
                                             <Typography variant="caption" sx={{ flex: 1, ml: '10px', textAlign: "left" }}>1. This registration is only for Alternative Investment Fund (AIF) registered in India. </Typography> <br></br>
                                             <Typography variant="caption" sx={{ flex: 1, ml: '10px', textAlign: "left" }}>2. Startups are not eligible to apply here.</Typography> <br></br>
                                             <Typography variant="caption" sx={{ flex: 1, ml: '10px', textAlign: "left" }}>3. Use your corporate e-mail address for sign up. A link to set your password will be sent.</Typography> <br></br>
-                                            <Typography variant="caption" sx={{ flex: 1, ml: '10px', textAlign: "left" }}>4. Temporary credentials will be active ony for 90 days.</Typography> <br></br>
+                                            <Typography variant="caption" sx={{ flex: 1, ml: '10px', textAlign: "left" }}>4. Temporary credentials will be active only for 90 days.</Typography> <br></br>
                                         </div>
 
                                         <div style={{paddingTop: 25}}>
