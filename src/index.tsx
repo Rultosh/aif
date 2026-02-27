929c3f27b26b5474fd9b3af2ad6d50dd6976cfac
ganesan <ganesan@ganesans-MacBook-Pro-2.local>
2026-02-27 11:37:29 +0530

    htaccess file added

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <HashRouter basename='/vcf'> 
         <App />
       </HashRouter>
     </Provider>
