cb4737574acf3b47f3db57f4e66997ac1b9f6464
ganesan <ganesan@ganesans-MacBook-Pro-2.local>
2026-02-27 13:17:43 +0530

    htaccess issue

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter basename='/vcf'> 
+      <HashRouter> 
         <App />
       </HashRouter>
     </Provider>
