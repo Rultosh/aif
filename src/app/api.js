7052c1cdc9329c86270daba747a399387dc04e73
judejegan-git <judejegan1986@gmail.com>
2026-06-08 12:52:54 +0530

    added # for proper redirection in prod

--- src/app/api.js
+++ src/app/api.js
@@ -64,7 +64,7 @@ client.interceptors.response.use(function (response) {
     }
     localStorage.clear();
     CheckAuth.setIsUnauthorized();
-    window.location.href = '/aif/portal/login';
+    window.location.href = '/aif/portal/#/login';
   }
   
   return Promise.reject(error);
